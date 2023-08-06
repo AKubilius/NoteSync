c798b5179ed88bf106cde2f6d9a664a0d683ffdc
AKubilius <dragaz123@gmail.com>
2023-08-06 15:11:16 +0300

    Small changes

--- app/src/main/java/com/android/notesync/MainActivity.kt
+++ app/src/main/java/com/android/notesync/MainActivity.kt
@@ -2,15 +2,18 @@ package com.android.notesync
 
 import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import android.graphics.Color
 import android.graphics.drawable.GradientDrawable
 import android.util.Log
 import android.view.Gravity
+import android.view.inputmethod.EditorInfo
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.EditText
+import android.widget.ImageButton
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
@@ -26,52 +29,48 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-
         addName()
 
-        val button: Button = findViewById<Button>(R.id.button)
+
         val textView: EditText = findViewById(R.id.textView)
 
-        button.setOnClickListener{
-            updateTextView(textView)
+        textView.setOnEditorActionListener { view, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                updateTextView(textView)
+                return@setOnEditorActionListener true
+            }
+            return@setOnEditorActionListener false
         }
 
+        val containerLayout: LinearLayout = findViewById(R.id.containerLayout)
+        val addButton: ImageButton = findViewById(R.id.buttonAdd)
+        val editTextManager = EditTextManager(this)
+        addButton.setOnClickListener {
+            editTextManager.addNewEditText(containerLayout)
+
+        }
 
         myRef.addValueEventListener(object: ValueEventListener {
-            val TAG = "Firebase"
             override fun onDataChange(snapshot: DataSnapshot) {
                 val value = snapshot.getValue()
                 updateText(textView,value.toString())
-                Log.d(TAG, "Value is: " + value)
             }
             override fun onCancelled(error: DatabaseError) {
-                Log.w(TAG, "Failed to read value.", error.toException())
             }
         })
 
     }
+    private fun updateTextView( textView: TextView) {
+        myRef.setValue(textView.text.toString()) }
 
-
-
-    private fun updateTextView( textView: TextView)
-    {
-        myRef.setValue(textView.text.toString())
-    }
-
-    private fun updateText(textView: TextView, value: String)
-    {
-        textView.text = value
-
-    }
+    private fun updateText(textView: TextView, value: String) {
+        textView.text = value }
 
     fun addName()
     {
         var loginName = intent.getStringExtra("loginName")
     }
 }
-
-
-
 
 class EditTextManager(private val context: Context) {
 
@@ -80,7 +79,6 @@ class EditTextManager(private val context: Context) {
         applyEditTextStyle(newEditText)
         addEditTextToContainer(newEditText, containerLayout)
     }
-
     private fun createNewEditText(): EditText {
         val newEditText = EditText(context)
         newEditText.textSize = 16f
@@ -90,13 +88,10 @@ class EditTextManager(private val context: Context) {
     }
 
     private fun applyEditTextStyle(editText: EditText) {
-        val shape = GradientDrawable()
-        shape.shape = GradientDrawable.RECTANGLE
-        shape.cornerRadius = context.resources.getDimensionPixelSize(R.dimen.rounded_corner).toFloat()
-        shape.setColor(Color.parseColor("#87CEFA"))
-
-        editText.background = shape
+        editText.setBackgroundColor(Color.WHITE) // Set background color to white
         editText.setPadding(16, 16, 16, 16)
+        editText.elevation = context.resources.getDimension(R.dimen.elevation)
+
     }
 
     private fun addEditTextToContainer(editText: EditText, containerLayout: LinearLayout) {
@@ -104,7 +99,11 @@ class EditTextManager(private val context: Context) {
             LinearLayout.LayoutParams.MATCH_PARENT,
             LinearLayout.LayoutParams.WRAP_CONTENT
         )
-        layoutParams.setMargins(0, 0, 0, context.resources.getDimensionPixelSize(R.dimen.margin_bottom))
+        layoutParams.setMargins(context.resources.getDimensionPixelSize(R.dimen.margin_left),
+            0,
+            context.resources.getDimensionPixelSize(R.dimen.margin_right),
+            context.resources.getDimensionPixelSize(R.dimen.margin_bottom))
+
         editText.layoutParams = layoutParams
         containerLayout.addView(editText)
     }
